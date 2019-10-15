2b76eb3f74bb66d94d0c131456fd3e95a9d11d67
Tamik <hello@Tamik.ru>
2019-10-16 00:07:00 +0300

    Minor updates

--- NetworkLayer/Encoding/URLParameterEncoder.swift
+++ NetworkLayer/Encoding/URLParameterEncoder.swift
@@ -14,22 +14,22 @@ public struct URLParameterEncoder: ParameterEncoder {
         guard let url = urlRequest.url else {
             throw NetworkLayerError.missingURL
         }
-        
+
         if var urlComponents = URLComponents(url: url,
                                              resolvingAgainstBaseURL: false), !parameters.isEmpty {
             urlComponents.queryItems = [URLQueryItem]()
-            
+
             for (key, value) in parameters {
                 let queryItem = URLQueryItem(name: key,
                                              value: "\(value)".addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)
                 )
-                
+
                 urlComponents.queryItems?.append(queryItem)
             }
-            
+
             urlRequest.url = urlComponents.url
         }
-        
+
         if urlRequest.value(forHTTPHeaderField: "Content-Type") == nil {
             urlRequest.setValue("application/x-www-form-urlencoded; charset=UTF-8", forHTTPHeaderField: "Content-Type")
         }
--- NetworkLayer/Service/HTTPTask.swift
+++ NetworkLayer/Service/HTTPTask.swift
@@ -13,12 +13,12 @@ public typealias HTTPHeaders = [String:String]
 
 public enum HTTPTask {
     case request
-    
+
     case requestParameters(
         bodyParameters: Parameters?,
         urlParameters: Parameters?
     )
-    
+
     case requestParametersAndHeaders(
         bodyParameters: Parameters?,
         urlParameters: Parameters?,
