9020706bec530ec929b06add5a7970cd85e326f4
Tamik <hello@Tamik.ru>
2019-10-16 00:05:47 +0300

    ContentType support in JSONParameterEncoder

--- NetworkLayer/Encoding/JSONParameterEncoder.swift
+++ NetworkLayer/Encoding/JSONParameterEncoder.swift
@@ -15,6 +15,10 @@ public struct JSONParameterEncoder: ParameterEncoder {
             let jsonAsData = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted)
 
             urlRequest.httpBody = jsonAsData
+            
+            if urlRequest.value(forHTTPHeaderField: "Content-Type") == nil {
+                urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            }
         } catch {
             throw NetworkLayerError.encodingFailed
         }
