670ed1e1f11c4c0287b6968ac66157c741f0f772
Tamik <hello@Tamik.ru>
2019-10-15 07:10:53 +0300

    JSONParameterEncoder

---
+++ NetworkLayer/Encoding/JSONParameterEncoder.swift
@@ -0,0 +1,16 @@
+//
+//  JSONParameterEncoder.swift
+//  NetworkLayer
+//
+//  Created by Tamerlan Lokyaev on 15.10.2019.
+//  Copyright © 2019 Tamik. All rights reserved.
+//
+
+import Foundation
+
+
+public struct JSONParameterEncoder: ParameterEncoder {
+    public static func encode(urlRequest: inout URLRequest, with parameters: Parameters) throws {
+        <#code#>
+    }
+}
