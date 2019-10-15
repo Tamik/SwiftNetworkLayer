98ebad415dc7dd4d2caecec2bb871e5a670eb68f
Tamik <hello@Tamik.ru>
2019-10-16 00:02:07 +0300

    URLParameterEncoder implementation

--- NetworkLayer/Encoding/URLParameterEncoder.swift
+++ NetworkLayer/Encoding/URLParameterEncoder.swift
@@ -11,6 +11,23 @@ import Foundation
 
 public struct URLParameterEncoder: ParameterEncoder {
     public static func encode(urlRequest: inout URLRequest, with parameters: Parameters) throws {
-        <#code#>
+        guard let url = urlRequest.url else {
+            throw NetworkLayerError.missingURL
+        }
+        
+        if var urlComponents = URLComponents(url: url,
+                                             resolvingAgainstBaseURL: false), !parameters.isEmpty {
+            urlComponents.queryItems = [URLQueryItem]()
+            
+            for (key, value) in parameters {
+                let queryItem = URLQueryItem(name: key,
+                                             value: "\(value)".addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)
+                )
+                
+                urlComponents.queryItems?.append(queryItem)
+            }
+            
+            urlRequest.url = urlComponents.url
+        }
     }
 }
