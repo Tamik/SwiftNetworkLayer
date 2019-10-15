c4050670947b6bf45697377ea95a49c71094918a
Tamik <hello@Tamik.ru>
2019-10-15 07:02:57 +0300

    HTTPMethod Service enum

---
+++ NetworkLayer/Service/HTTPMethod.swift
@@ -0,0 +1,18 @@
+//
+//  HTTPMethod.swift
+//  NetworkLayer
+//
+//  Created by Tamerlan Lokyaev on 15.10.2019.
+//  Copyright © 2019 Tamik. All rights reserved.
+//
+
+import Foundation
+
+
+public enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case patch = "PATCH"
+    case delete = "DELETE"
+}
