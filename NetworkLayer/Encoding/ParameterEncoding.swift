a4b525f5d03a0d2642f6e3eac81311f993a3ca3f
Tamik <hello@Tamik.ru>
2019-10-15 07:08:31 +0300

    NetworkLayerError

--- NetworkLayer/Encoding/ParameterEncoding.swift
+++ NetworkLayer/Encoding/ParameterEncoding.swift
@@ -14,3 +14,9 @@ public typealias Parameters = [String:Any]
 public protocol ParameterEncoder {
     static func encode(urlRequest: inout URLRequest, with parameters: Parameters) throws
 }
+
+public enum NetworkLayerError {
+    case parametersNil = "Parameters were nil."
+    case encodingFailed = "Parameter encoding failed."
+    case missingURL = "URL is nil."
+}
